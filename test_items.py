aafbe5f3e1faa8712fb3a093814f5b5bb400621e
Дарья Гимпель <gimpel@devel.ifx>
2020-05-16 14:37:31 +0300

    add file test_items

---
+++ test_items.py
@@ -0,0 +1,11 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_check_busket_button(browser):
+    browser.get(link)
+    
+    browser.implicitly_wait(10)
+    
+    button = browser.find_element_by_css_selector("button.btn-add-to-basked")
+    
+    assert button, "Корзина не найдена "
+   
